aeacb0b0870bf1aa7a1746e310c8aea48c3aeeb5
TheTrollGodJr <69094231+TheTrollGodJr@users.noreply.github.com>
2024-02-21 16:01:55 -0700

    Update getUpdate.py

--- pythonFiles/getUpdate.py
+++ pythonFiles/getUpdate.py
@@ -1,6 +1,7 @@
 import requests
 import shutil
 import zipfile
+import subprocess
 
 '''
     Downloads the code from the specified github page to the local computer
@@ -20,6 +21,21 @@ def transfer_files():
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall("VOID")
 
+def get_packages():
+    with open('VOID/requirements.txt', 'r') as file:
+        requirements = file.readlines()
+    
+    for requirement in requirements:
+        subprocess.call(['pip', 'install', requirement.strip()])
+
+def launch_program(program_path, arg, filetype):
+    try:
+        subprocess.Popen([filetype, program_path] + arg)
+    except subprocess.CalledProcessError as e:
+        print(f"Error Launching Program: {e}")
+
 if __name__ == "__main__":
     download_file("https://github.com/TheTrollGodJr/VOID/archive/refs/heads/main.zip", "VOID/VOID.zip")
     transfer_files()
+    get_packages()
+    launch_program("VOID/VOID.pyw", [], "pythonw")
